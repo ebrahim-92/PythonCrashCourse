3cfd25844ae87ad0bfb432f2700d25f6213c4481
ebrahim-92 <ebrahimalbasry@ymail.com>
2023-12-14 13:51:42 -0600

    Updated hello-world file

---
+++ simple_message.py
@@ -0,0 +1,12 @@
+# Try it yourself section of Variables and Simple Data Types chapter
+
+# Simple Message 2.1
+
+var1 = "Python is fun"
+print(var1)
+
+# Simple message 2.2
+var2 = "Python is fun"
+var2 = "Learning it is easy"
+
+print(var2)
