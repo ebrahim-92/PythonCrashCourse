13f468314ba451b2393916234c83e6414d42e59b
ebrahim-92 <ebrahimalbasry@ymail.com>
2023-12-14 13:33:20 -0600

    Updated hellow-world file

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,11 @@
-# First print statement
+# First pringt statement
 
-print("Hellow World!")
+print("Hello world!")
+
+# First use of variable being printed
+
+message = "Hello world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
