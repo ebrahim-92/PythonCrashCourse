47bfd62fd7bd1f3adc43caa5da5b5558ef4f491a
ebrahim-92 <ebrahimalbasry@ymail.com>
2023-12-15 11:57:02 -0600

    # Authored by Ebrahim Al-basry

---
+++ lists_exercises/bicycles.py
@@ -0,0 +1,13 @@
+# Lists basic exercises
+
+bicycles = ["trek", "canondale", "redline", "specialized"]
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[-1])
+
+# Individual values from lists
+
+bicycles = ["trek", "canondale", "redline", "specialized"]
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
---
+++ lists_exercises/names.py
@@ -0,0 +1,8 @@
+# Names 3.1
+
+names = ["Salih", "Muhammad", "Ali", "Isa", "Musa"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
