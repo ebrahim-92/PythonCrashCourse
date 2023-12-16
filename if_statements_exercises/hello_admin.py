11db5518e0e7bcaf5cff0e8a5cc02aced7ae85b0
ebrahim-92 <ebrahimalbasry@ymail.com>
2023-12-16 14:54:03 -0600

    # Initial commit

--- if_statements_exercises/hello_admin.py
+++
@@ -1,9 +0,0 @@
-# Hello admin 5.8
-
-usernames = ["kratos", "admin", "altair", "dragonborn", "geralt"]
-
-for username in usernames:
-    if username == "admin":
-        print(f"Hello {username.lower()}, would you like to see a status report?")
-    else:
-        print(f"Hello {username.title()} thanks for logging in today!")
